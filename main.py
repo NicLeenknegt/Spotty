e3e088b4e50e600f941691dbb2ae1dcce2792af6
Nic Leenknegt <Nicolaas_Leenknegt>
2020-08-12 18:52:22 +0200

    cleaned up deno tryout, threading tests and remove bash file

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
 #!/usr/bin/python3
 
-import sys,  getopt, urllib.parse, webbrowser, time, concurrent.futures, queue, threading
-from typing import Union, Callable
-from data.api_caller import set_url,add_query_parameters
-from views.MainView import MainView
-from enum import Enum
+import sys,  getopt
 from views.DeviceView import DeviceView
 from view_models.DeviceViewModel import DeviceViewModel
 
@@ -20,19 +16,6 @@ def main(argv):
             device_view = DeviceView(view_model) 
             device_view.show_device_table()
             sys.exit()
-
-@set_url("localhostt")
-@add_query_parameters()
-def get_client_config()-> dict:
-    print("Welcome to Spotty!")
-    print("To start the config you need a client id and a client secret.")
-    print("You can find these on your app on the spotify dashboard.")
-    print("If you do not already have a developer account you can make one by going to https://developer.spotify.com/dashboard/login .")
-    print("Afterwards go to your dashboard and create an app.")
-    print("You find your client id and client secret there.")
-    client_id:str = input("CLIENT_ID:")
-    client_secret:str = input("CLIENT_SECRET:")
-    return { 'CLIENT_ID':client_id, 'CLIENT_SECRET':client_secret }
 
 if  __name__ ==  "__main__":
     main(sys.argv[1:])
--- views/SetupView.py
+++ views/SetupView.py
@@ -1,6 +1,20 @@
 
+from data.api_caller import set_url,add_query_parameters
+
 class SetupView:
 
     def __init__(self):
-        
-    
+        self.check =  None
+
+    @set_url("localhostt")
+    @add_query_parameters()
+    def get_client_config(self)-> dict:
+        print("Welcome to Spotty!")
+        print("To start the config you need a client id and a client secret.")
+        print("You can find these on your app on the spotify dashboard.")
+        print("If you do not already have a developer account you can make one by going to https://developer.spotify.com/dashboard/login .")
+        print("Afterwards go to your dashboard and create an app.")
+        print("You find your client id and client secret there.")
+        client_id:str = input("CLIENT_ID:")
+        client_secret:str = input("CLIENT_SECRET:")
+        return { 'CLIENT_ID':client_id, 'CLIENT_SECRET':client_secret }
